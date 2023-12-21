398ec3e2a76f07c64f1ba6a2771f79a10dee392d
Jack <jconnolly444@gmail.com>
2023-12-21 14:29:26 -0600

    added models for Artist, Album and User; added has_many/belongs_to relationships between Artist and Album

---
+++ db/migrate/20231221202047_create_artists.rb
@@ -0,0 +1,12 @@
+class CreateArtists < ActiveRecord::Migration[7.0]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :image_url
+      t.text :bio
+      t.string :genre
+
+      t.timestamps
+    end
+  end
+end
